fe06e8529321343b54bbf023fe2c82ac981c002d
patricia.leite <patricia.leite>
2017-02-02 16:54:22 -0200

    init project

---
+++ js/settings.js
@@ -0,0 +1,74 @@
+"use strict"
+/***********************************************
+ * slider.js
+ * company: areaH.com.br
+ * developer: Poltts <patricia.leite@marketup.com>
+************************************************/
+
+var headerH     = $("header").css("height");
+var pageH       = $(".content-page").css("height");
+var totalPage   = (headerH.replace('px', '') ) + (pageH.replace('px', '') );
+var newPageH    = parseInt(totalPage) + parseInt(1200) + 'px';
+
+
+var loading = setInterval(function(){
+
+    // Loaded content check
+    if(document.readyState === 'complete') {
+        clearInterval(loading);
+        
+        $(window).scrollTop(0);
+        $(".content").show();
+        $(".loading").hide();
+
+        //init sliders
+        slider("#slider-sapatos");
+        slider("#bizarrices-nos-pes-slide");
+        
+        $(".navbar-toggle").click(function(event) {
+            $(".navbar-collapse").collapse('show'); 
+            $(".navbar-collapse").addClass('normalize'); 
+        });
+
+        $('.nav a').on('click', function() {
+            $('.navbar-collapse').collapse('hide');
+        });
+
+        if(window.innerWidth > 320){
+            $(".unsupported").show();
+        }
+
+    }else{
+        $(".loading").show();
+    }
+
+},100);
+
+
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".collapse").removeClass("normalize");
+        $(".icon-top").show();
+        $("#brand").show();
+         $(".navbar-collapse").collapse('hide'); 
+
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".collapse").addClass("normalize");
+        $(".icon-top").hide();
+        $("#brand").hide();
+    }
+});
+
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
---
+++ js/slider.js
@@ -0,0 +1,55 @@
+/*********************************************** 
+ * slider.js
+ * company: areaH.com.br
+ * developer: Poltts <patricia.leite@marketup.com> 
+************************************************/
+
+ function slider(theSlider){
+
+        var images = $(theSlider+" figure").children();
+        var imgsTotal = images.length;
+        var texto = null;
+
+            $(images).each(function(){
+                if(images != "figcaption"){
+                    $(images).hide();
+                    $(images[0]).addClass("on").show();
+                }
+                texto = $(images).attr("title");
+                $(theSlider+" figure > figcaption").html("<p>"+texto+"</p>");
+               
+            });
+            
+            $(theSlider+" figure > figcaption").show();
+            $(".prev").hide();
+            $(".next").show();
+
+            $(".next").on("click", function(){
+                if($(theSlider+" figure > img.on").next().attr("src")){
+                    console.log(1);
+                    $(".next").show();
+                    $(".prev").show();
+                    $(theSlider+" figure > img.on").fadeOut().removeClass("on").next().fadeIn().addClass("on");
+                    texto = $(theSlider+" img.on").attr("title");
+                    $(theSlider+" figure > figcaption").html("<p>"+texto+"</p>");
+                }
+               
+            });
+           
+            $(".prev").on("click", function(){
+                // if($(images[0]).attr("class") === "on"){
+                //      $(".prev").hide();
+                //      $(".next").show();
+                // }
+                if($(theSlider+" figure > img.on").prev().attr("src")){
+                    $(".next").show();
+                    $(".prev").show();
+                        $(theSlider+" img.on").fadeOut().removeClass("on").prev().fadeIn().addClass("on");
+                        texto = $(theSlider+" img.on").attr("title");
+                        $(theSlider+" figure > figcaption").html("<p>"+texto+"</p>");
+                }
+
+            });
+
+        
+    }
